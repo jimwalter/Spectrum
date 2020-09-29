4eaacbe2266332b9de5688677b8c08d98d85be9a
jim walter <jimwalter13090@gmail.com>
2020-09-29 10:54:56 -0500

    map sample JSON into table

--- challenge/src/App.js
+++ challenge/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import data from "./data.json";
+import Table from "./components/Table.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fetchResults: [],
+      sortedResults: [],
+      sortTerm: "",
+    };
+  }
+  componentDidMount() {}
+  getResults() {}
+  updateSortTerm() {}
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Hello</p>
+        </header>
+        <body>
+          <Table restaurants={data} />
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ challenge/src/components/Table.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Table(props) {
+  const { restaurants } = props;
+  return (
+    <table>
+      <caption>Our restaurants</caption>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>City</th>
+          <th>State</th>
+          <th>Phone Number</th>
+          <th>Genres</th>
+        </tr>
+      </thead>
+      <tbody>
+        {restaurants.map((restaurant) => (
+          <tr key={restaurant.id}>
+            <td>{restaurant.name}</td>
+            <td>{restaurant.city}</td>
+            <td>{restaurant.state}</td>
+            <td>{restaurant.telephone}</td>
+            <td>{restaurant.genre}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
